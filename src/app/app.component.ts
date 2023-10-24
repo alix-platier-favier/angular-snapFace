95e20b655e75d1f48ccbb00b6aa58b433bbbfc06
Alix Platier-Favier <alix.platier-favier@groupeadf.com>
2023-10-24 13:23:19 +0200

    Start new project - Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  interval$!: Observable<number>;
+
+  ngOnInit() {
+      this.interval$ = interval(1000).pipe(
+        map(value => value * 10)
+      ); 
+  }
+}
